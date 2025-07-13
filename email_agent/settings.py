036a325d367c38165b55bbdbb02805a7c7cc4bf2
MARUTI PAI <marutipai203@gmail.com>
2025-07-13 14:38:40 +0530

    fixed allowed hosts

--- email_agent/settings.py
+++ email_agent/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-ae!vl5xe&s5szba71qe(1qig87036*-^t0_&*(*515ui#zn9^(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['email-agent-bjsy.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
